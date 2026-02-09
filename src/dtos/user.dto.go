44938803d1d2353e5ce79747fa5af11afe66ca02
yuusufyan <yuusufyan13@gmail.com>
2026-02-10 04:05:44 +0700

    REFACTOR : Refactor param response

--- src/dtos/user.dto.go
+++ src/dtos/user.dto.go
@@ -3,11 +3,11 @@ package dtos
 type UserCreateRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	UserName string `json:"user_name"`
+	UserName string `json:"username"`
 }
 
 type UserResponse struct {
 	ID       int    `json:"id"`
 	Email    string `json:"email"`
-	UserName string `json:"user_name"`
+	UserName string `json:"username"`
 }
